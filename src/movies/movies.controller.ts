44e2d79e0e5dc3a61e169c318498345a05a16695
bioroid17 <trace1997@ajou.ac.kr>
2026-09-18 03:28:41 +0900

    REST API - Movies Service part 2

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -30,15 +30,12 @@ export class MoviesController {
   }
 
   @Delete(':id')
-  remove(@Param('id') movieId: string): boolean {
-    return this.moviesService.deleteOne(movieId);
+  remove(@Param('id') movieId: string) {
+    this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
   patch(@Param('id') movieId: string, @Body() updateData: any) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+    return this.moviesService.update(movieId, updateData);
   }
 }
